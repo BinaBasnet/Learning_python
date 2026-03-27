33aadf9534eb054dd5a9e1c0f08d524e9cb688c4
BinaBasnet <binabasnet132@gmail.com>
2026-03-27 19:06:52 +0545

    Add basic arithmetic operations and modulo example

--- Integers_and_floats_operations/Integers_and_floats_operations.py
+++ Integers_and_floats_operations/Integers_and_floats_operations.py
@@ -1,13 +1,17 @@
 # Declartion
 a= 5
 b= 6
+c= 10
 my_float= 5.6
 
 # To check the type
 print(type(my_float))
 
 # For addition
-
+# TODO: this is just an example 
 print(a+b)
 print(a-b)
-print(a+my_float)
+print(a+c)
+
+# Modular operator
+print(10%3)
